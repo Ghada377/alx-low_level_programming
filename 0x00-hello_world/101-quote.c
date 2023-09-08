1f90a98d14b6ecfb693d715c647caf4814798d75
ghada377 <ghadaabdelhady77@gmail.com>
2023-09-07 19:50:21 -0700

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+    char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, quote, 59); /* Using write to print to stderr */
+
+    return (1);
+}
