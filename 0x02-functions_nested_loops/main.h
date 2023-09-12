9f416d26374ee263c37e9bc18ea060ded6ef1e1a
ghada377 <ghadaabdelhady77@gmail.com>
2023-09-12 15:43:48 -0700

    1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,8 @@
 /* Function prototype for _putchar */
 int _putchar(char c);
 
+/* Prototype for print_alphabet */
+void print_alphabet(void);
 /* Add any other function prototypes here */
 
 #endif /* MAIN_H */
