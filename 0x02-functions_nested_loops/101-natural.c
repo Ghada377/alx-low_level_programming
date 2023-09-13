cda5c488aea22eb486398c6a87377135cbe9ea44
ghada377 <ghadaabdelhady77@gmail.com>
2023-09-12 18:39:15 -0700

    13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,11 +5,12 @@
  *
  * Return: Always 0
  */
+
 int main(void)
 {
 	int sum, num;
 
-	for (num = 0; num <= 1024; num++)
+	for (num = 0; num < 1024; num++)
 	{
 		if ((num % 3 == 0) || (num % 5 == 0))
 			sum += num;
