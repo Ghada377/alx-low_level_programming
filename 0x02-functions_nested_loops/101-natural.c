b24b1589db3a688ef324fc3b323e34275991a328
ghada377 <ghadaabdelhady77@gmail.com>
2023-09-12 18:36:36 -0700

    13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,29 +7,14 @@
  */
 int main(void)
 {
-        int i;
-        long int fib1 = 1, fib2 = 2, sum = 0;
+	int sum, num;
 
-        for (i = 1; i <= 32; i++)
-        {
-                if (fib2 % 2 == 0)
-                        sum += fib2;
-                fib2 = fib1 + fib2;
-                fib1 = fib2 - fib1;
-        }
-
-        _putchar((sum / 1000000000) + '0');
-        _putchar(((sum / 100000000) % 10) + '0');
-        _putchar(((sum / 10000000) % 10) + '0');
-        _putchar(((sum / 1000000) % 10) + '0');
-        _putchar(((sum / 100000) % 10) + '0');
-        _putchar(((sum / 10000) % 10) + '0');
-        _putchar(((sum / 1000) % 10) + '0');
-        _putchar(((sum / 100) % 10) + '0');
-        _putchar(((sum / 10) % 10) + '0');
-        _putchar((sum % 10) + '0');
-        _putchar('\n');
-
-        return (0);
+	for (num = 0; num <= 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
 
