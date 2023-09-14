d936ec10e3adf65c0c9d746a68c25943f847b19b
ghada377 <ghadaabdelhady77@gmail.com>
2023-09-14 14:44:54 -0700

    0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/* Function prototype for 0-isupper.c */
+int _isupper(int c);
+
+#endif /* MAIN_H */
+
